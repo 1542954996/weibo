4fb0a6d550496aa027ba71539b6951b2c7b15543
XB <1542954996@qq.com>
2024-02-08 16:35:18 +0800

    命名路由

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -13,7 +13,7 @@
         </a>
 
         <div class="float-end">
-            <a href="/about" class="text-decoration-none">关于</a>
+            <a href="{{route('about')}}" class="text-decoration-none">关于</a>
         </div>
     </footer>
 </body>
