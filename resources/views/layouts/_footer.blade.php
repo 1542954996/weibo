581984a79227d59b583e62d2c0dc88b7d6e71d5e
XB <1542954996@qq.com>
2024-02-08 16:25:45 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <footer class ="footer">
+        <img class="brand-icon" src="https://cdn.learnku.com/uploads/sites/KDiyAbV0hj1ytHpRTOlVpucbLebonxeX.png">
+        <a href="https://learnku.com/laravel/courses" target=_blank class="text-decoration-none">
+            刻意练习，每日精进
+        </a>
+
+        <div class="float-end">
+            <a href="/about" class="text-decoration-none">关于</a>
+        </div>
+    </footer>
+</body>
+</html>
