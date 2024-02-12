685ad726ebed4e1609184b17e673870050add4ff
XB <1542954996@qq.com>
2024-02-12 15:51:16 +0800

    访问策略

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -22,5 +22,10 @@ class AuthServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+        $this->registerPolicies();
+        //修改策略自动发现的逻辑
+        Gate::guessPolicyNamesUsing(function ($modelClass){
+            return 'App\Policies\\'.class_basename($modelClass).'Policy';
+        });
     }
 }
